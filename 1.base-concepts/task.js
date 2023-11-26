4ac5191ae0a977db04a2f93967b9121b30f3d89d
neondoll <bonni_1998@mail.ru>
2023-11-26 17:29:43 +0300

    Первое задание полностью готово

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -1,10 +1,33 @@
 "use strict"
+
 function solveEquation(a, b, c) {
   let arr = [];
-  
+  // Вычисление дискриминанта
+  const discriminant = b ** 2 - 4 * a * c;
+
+  // Вычисление корней
+  switch (true) {
+    case discriminant > 0:
+      arr.push((-b + Math.sqrt(discriminant)) / (2 * a));
+      arr.push((-b - Math.sqrt(discriminant)) / (2 * a));
+      break;
+    case discriminant === 0:
+      arr.push(-b / (2 * a));
+      break;
+  }
+
   return arr;
 }
 
 function calculateTotalMortgage(percent, contribution, amount, countMonths) {
-  
+  // Преобразование процентной ставки
+  const monthlyPercent = percent / 100 / 12;
+  // Расчет тела кредита
+  const loanBody = amount - contribution;
+  // Расчет ежемесячной оплаты
+  const payment = loanBody * (monthlyPercent + (monthlyPercent / (((1 + monthlyPercent) ** countMonths) - 1)));
+  // Расчет общей суммы
+  const totalAmount = payment * countMonths;
+
+  return Number(totalAmount.toFixed(2));
 }
